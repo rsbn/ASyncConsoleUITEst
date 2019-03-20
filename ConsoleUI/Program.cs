0ca657751cbafb935168eb18c5d675f8a78eb658
rsbn <paulo@rsbn.com.br>
2019-03-19 21:02:13 -0300

    Initial

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleUI
@@ -10,6 +11,35 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            Worker worker = new Worker();
+            worker.DoWork();
+            while (!worker.Finished)
+            {
+                Console.Write("z");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("\nFinished!");
+            Console.ReadLine();
+        }
+    }
+
+    public class Worker
+    {
+        public bool Finished { get; set; } = false;
+        public async void DoWork()
+        {
+            Console.WriteLine("Starting");
+            await TakeSomeRest();
+            Finished = true;
+        }
+
+        private Task TakeSomeRest()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Sleeping ...");
+                Thread.Sleep(4000);
+            });
         }
     }
 }
